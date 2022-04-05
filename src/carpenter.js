e0520718cfe53ead939916f85efef10ce80f3866
mattee <admin@mattee.net>
2022-04-05 21:09:13 +0200

    Changed loadScript to loadScripts.

--- src/carpenter.js
+++ src/carpenter.js
@@ -4,7 +4,7 @@ window.onload = function() {
     var importModuleTag = document.createElement('script');
     importModuleTag.src = 'https://mattee.net/scripts/carpenterjs/core/import.js';
     importModuleTag.onload = async function(){
-        await loadScript("util/strings");
+        await loadScripts(["util/strings"]);
         loadScript("scripts/init.js?version=" + createRandomString(10));
     }
     document.head.appendChild(importModuleTag);
