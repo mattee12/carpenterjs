f0fddb8bd6ca0884c63d7feb6979ad2a74baf374
mattee <admin@mattee.net>
2022-04-06 21:12:19 +0200

    Trying solution.

--- src/engine/elements/dialog/dialog.js
+++ src/engine/elements/dialog/dialog.js
@@ -13,3 +13,7 @@ registerWaitingForLoad(function(){
     document.body.appendChild(BACKDROP);
     applyStyle(BACKDROP, {"z-index": "99", "opacity": "0", "visibility": "hidden", "background-color": "rgba(0, 0, 0, 0.35)", "width": "100vw", "height": "100vh", "position": "fixed", "transition": "opacity .2s, visibility .2s"});
 });
+
+class Dialog extends Element{
+    
+}
