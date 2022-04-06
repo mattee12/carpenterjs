1243a10019cbc075d7f9016b1cdea93f307595df
mattee <admin@mattee.net>
2022-04-06 20:35:55 +0200

    Added common properties to Element.

--- src/engine/elements/element.js
+++ src/engine/elements/element.js
@@ -2,10 +2,26 @@ class Element {
     constructor(e, p){
         this.e = e;
         this.p = p;
+        this.type = p["type"];
         this.#handleProperties();
     }
 
     #handleProperties(){
-    
+        delete this.p["type"];
+        Object.keys(this.p).forEach((k) => {
+            switch(k){
+                case "w":
+                case "width":
+                    applyStyle(el, {"width": this.p[k]});
+                    break;
+                case "h":
+                case "height":
+                    applyStyle(el, {"height": this.p[k]});
+                    break;
+                case "fadein":
+                    applyStyle(el, {"animation": "fadein .25s linear"});
+                    break;
+            }
+        });
     }
 }
--- src/engine/elements/icons/icon.js
+++ src/engine/elements/icons/icon.js
@@ -12,24 +12,13 @@ ICON_TYPES.forEach((type) => {
 
 class Icon extends Element{
     constructor(e, p){
-        this.e = e;
-        this.p = p;
-        this.type = p["type"];
+        super(e, p);
     }
     deploy(){
         return new Promise(async resolve => {
             var el = await elementFromUrl("https://mattee.net/" + ICON_PATH[this.p["type"]]);
-            delete this.p["type"];
             Object.keys(this.p).forEach((k) => {
                 switch(k){
-                    case "w":
-                    case "width":
-                        applyStyle(el, {"width": this.p[k]});
-                        break;
-                    case "h":
-                    case "height":
-                        applyStyle(el, {"height": this.p[k]});
-                        break;
                     case "color1":
                     case "color2": {
                         var colorElem = el.querySelector("#" + k);
@@ -40,12 +29,6 @@ class Icon extends Element{
                         debugPrint("Could not find " + k + " for " + this.type);
                         break;
                     }
-                    case "fadein":
-                        applyStyle(el, {"animation": "fadein .25s linear"});
-                        break;
-                    default:
-                        console.log("Invalid property for " + this.p["type"] + ": " + k);
-                        break;
                 }
             });
             this.e.appendChild(el);
