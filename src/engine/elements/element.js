00c7f69a9881f918e8a747f435502a20b539a246
mattee <admin@mattee.net>
2022-04-07 20:29:18 +0200

    Added width, height value shorthands.

--- src/engine/elements/element.js
+++ src/engine/elements/element.js
@@ -26,7 +26,12 @@ class Element {
                 case "h":
                 case "height": {
                     const k_long = k == "w" ? "width" : k == "h" ? "height" : k;
-                    applyStyle(this.e, {[k_long]: p[k]});
+                    const size = p[k] == "xs" ? "32px" :
+                    p[k] == "sm" ? "64px" :
+                    p[k] == "md" ? "128px" :
+                    p[k] == "lg" ? "256px" :
+                    p[k] == "xl" ? "512px" : null;
+                    applyStyle(this.e, {[k_long]: size ? size : p[k]});
                     break;
                 }
                 case "fadein":
